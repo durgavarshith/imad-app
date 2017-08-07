48c432422478a6da677fa4200da26dd236ab27aa
durgavarshith <durgavarshith@users.noreply.github.com>
2017-08-07 22:42:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,6 @@ button.onclick=function(){
     //create a request object
     var request=new XMLHttpRequest();
     
-    
     //capture response and store it in the counter value
     request.onreadystatechange=function(){
         if(request.readyState==XMLHttpRequest.DONE){
@@ -23,5 +22,22 @@ button.onclick=function(){
     //make the request
     request.open('GET','http://ratnavarshith.imad.hasura-app.io/counter',true);
     request.send(null);
+};
+
+//submit name
+var Input=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_bt');
+submit.onclick=function(){
+    //make a request to server and send the name
     
+    //capture a list of names and render it as a list
+    var names=['name1','name2','name3','name4'];
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list+='<li>'+names[i]+'</li>';
+    }
+    var ul=document.getElementById
+    ('namelist');
+    ul.innerHTML=list;
 };

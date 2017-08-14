97c8d0ea6ea9bf3daad5b7bb06647db266cf2b2c
durgavarshith <durgavarshith@users.noreply.github.com>
2017-08-14 21:46:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,6 +72,7 @@ app.get('/hash/:input',function(req,res){
 
 app.post('/create-user',function(req,res){
     //username,password
+    //{"username":"varshith",password:"password"}
     //JSON
     var username=req.body.username;
     var password=req.body.password;

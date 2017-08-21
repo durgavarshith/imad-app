b2b6da07649706115e66975d1f37470796f794ed
durgavarshith <durgavarshith@users.noreply.github.com>
2017-08-21 22:34:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,30 +1,5 @@
-//counter code
-var button=document.getElementById('counter');
 
-button.onclick=function(){
-    
-    //create a request object
-    var request=new XMLHttpRequest();
-    
-    //capture response and store it in the counter value
-    request.onreadystatechange=function(){
-        if(request.readyState===XMLHttpRequest.DONE){
-            //take some action
-            if(request.status===200){
-                var counter=request.responseText;
-               var span=document.getElementById('count');
-    span.innerHTML=counter.toString(); 
-            }
-        }
-        //not done yet
-    };
-    
-    //make the request
-    request.open('GET','http://ratnavarshith.imad.hasura-app.io/counter',true);
-    request.send(null);
-};
-
-//submit name
+//submit username/password to login
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
     
@@ -36,21 +11,23 @@ submit.onclick=function(){
          if(request.readystate===XMLHttpRequest.DONE){
             //take some action
             if(request.status===200){
-                 //capture a list of names and render it as a list
-    var names=request.responseText;
-    names=JSON.parse(names);
-    var list=''; 
-    for(var i=0;i<names.length;i++){
-        list+='<li>'+names[i]+'</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
+                alert('logged in sucessfully');
+            }else if(request.status===403){
+                alert('username/password is incorrect');
+            }else if(request.status===500){
+                alert('something went wrong on server');
             }
     }
     //not done yet
     };
     
     //make the request
-     request.open('GET','http://ratnavarshith.imad.hasura-app.io/submit-name?name='+name,true);
-    request.send(null);
+    var username=document.getElementById('username').value;
+    var password=document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+     request.open('POST','http://ratnavarshith.imad.hasura-app.io/login',true);
+     request.setRequest('content-Type','application/json');
+    request.send(JSON.stringify({username:username,password:password}));
+    
    };
